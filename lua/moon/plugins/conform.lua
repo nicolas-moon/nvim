ab0ab247053b99b982fae4a154174cf6b3cba320
Nicolas Moon <nmoon@lightspeedsystems.com>
2024-05-02 23:37:26 -0500

    stuff

---
+++ lua/moon/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+    'stevearc/conform.nvim',
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                go = { "goimports" },
+                typescript = { "eslint_d" },
+                javascript = { "eslint_d" },
+                rust = { "rustfmt" },
+
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            }
+        })
+
+        vim.api.nvim_set_keymap('n', '<leader>f', ':lua require("conform").format()<CR>',
+            { noremap = true, silent = true })
+    end
+
+}
